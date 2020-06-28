be3b04f811e8d5dc18daa05ef88b10072e835c50
sarahtan460 <57710110+sarahtan460@users.noreply.github.com>
2020-06-29 04:01:14 +0800

    Update reg-programs.component.ts

--- MPWebsite2(1)/src/app/reg-programs/reg-programs.component.ts
+++ MPWebsite2(1)/src/app/reg-programs/reg-programs.component.ts
@@ -16,7 +16,7 @@ export class RegProgramsComponent implements OnInit {
   programsArray: any = [];
   programEmpty:boolean;
   msg:string;
- 
+  orders:any=[];
   constructor(public postsService: PostsService,private route: ActivatedRoute, private toastr: ToastrService, private router: Router, public authService: AuthService) {
      this.postsService.getAllProgrammes().subscribe(programs => {
       for(var i = 0; i < programs.length; i++)
@@ -41,6 +41,12 @@ export class RegProgramsComponent implements OnInit {
       console.log(this.programsArray);
           
       });
+
+      this.postsService.getOrders(sessionStorage.getItem("userID")).subscribe(orders => {
+
+        this.orders = orders;
+  
+      });
      }
 
   ngOnInit(): void {
@@ -49,10 +55,28 @@ export class RegProgramsComponent implements OnInit {
   removeProgram(_id){
     let userName =  sessionStorage.getItem("LoggedIn");
     if(confirm('Are you sure that you want to cancel your registration for this program ?')){
-      this.postsService.removeFromPrograms(_id,userName).subscribe(results =>{
-        this.toastr.success("Successfully cancel your registration", 'Success!');
-      })
-      location.reload();
+      var currentdate= new Date().getDate();
+      for(var i =0; i<this.orders.length; i++)
+      {
+        if(this.orders[i].userID == sessionStorage.getItem("userID") && this.orders[i].itemID == _id)
+        {
+          var orderDate = new Date(this.orders[i].order_date).getDate()+2;
+          if(orderDate >= currentdate)
+          {
+           this.postsService.removeFromPrograms(_id,userName).subscribe(results =>{
+           this.toastr.success("Successfully cancel your registration", 'Success!');
+          })
+          location.reload();
+          
+          }
+         else {
+            alert('Sorry! You are only allow to cancel your program or workshop within 2 days from your order date');
+            return;
+          }
+         
+        }
+      }
+    
     }
   }
 
